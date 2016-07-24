302a64eed82959153a25ced149ff37b094b1f918
feng1991 <zhangyifengit@163.com>
2016-07-24 09:43:07 +0800

    增加声音提示功能；优化入参

--- clock/main.c
+++ clock/main.c
@@ -11,14 +11,18 @@ int main(){
     time_t rawtime;
 
     time(&rawtime);
-    printf("Current time : %sPlease input the times and time you want to alert: ",
+    printf("Current time : %sPlease input the times and seconds you want to alert: ",
            ctime(&rawtime));
     scanf("%d %d",&n,&l);
+    l = (l <= 0) ? 1 : l;
+    l = l * 1000;
+    n = (n <= 0) ? 1 : n;
     //printf("%d %d",n,l);exit(0);
     while(i < n){
         Sleep(l);
+        Beep(500,1000);
         MessageBox(NULL,"title","ok",MB_OK);
         i++;
     }
-    exit(0);
+    return 0;
 }
